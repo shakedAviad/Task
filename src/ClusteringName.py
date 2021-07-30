fb20a8631b30341d56d517a5f7c4c9a393a43576
shaked <shakedaviad55@gmail.com>
2021-07-30 16:18:09 +0300

    v2

--- src/ClusteringName.py
+++ src/ClusteringName.py
@@ -2,6 +2,7 @@ import time
 from threading import Thread, Lock
 import threading
 from fuzzywuzzy import fuzz
+from multiprocessing import Process
 
 
 class ClusteringName:
@@ -9,7 +10,6 @@ class ClusteringName:
         self.EOF = False
         self.befor_procesing = list()
 
-        self.after_procesing = list()
         self.groups = list()
         self.file_name = file_name
 
@@ -17,7 +17,6 @@ class ClusteringName:
 
         print("function input file ,{0}".format(threading.currentThread().getName()))
         with open(self.file_name, "r") as file:
-
             for name in file:
                 # print("function input file ,{0}".format(threading.currentThread().getName()))
                 with lock:
@@ -25,7 +24,7 @@ class ClusteringName:
 
         self.EOF = True
 
-    def processing(self, lock_befoer,lock_after):
+    def processing(self, lock):
 
         print("function processing  ,{0}".format(threading.currentThread().getName()))
         while True:
@@ -33,62 +32,44 @@ class ClusteringName:
             if self.EOF and len(self.befor_procesing) == 0: break
             if len(self.befor_procesing) == 0:continue
             else:
-                with lock_befoer:
-                    n = self.befor_procesing.pop()
-                
-                n = n.lower().replace(" ", "").replace("\n", "")
-                with lock_after:
-                    self.after_procesing.append(n)
-
-
-    def algorithm(self,lock):
-
-        print("function algorithm ,{0}".format(threading.currentThread().getName()))
-        grs=list()
-        while True:
-            # print("function algorithm ,{0}".format(threading.currentThread().getName()))
-            if self.EOF and len(self.after_procesing) == 0 and len(self.befor_procesing) == 0: break
-            if len(self.after_procesing) == 0:continue
-            else:
                 with lock:
-                    n = self.after_procesing.pop()
-                for group in self.groups:
-                    if all(fuzz.ratio(n,w)>80 for w in group):
-                        group.append(n)
-                        break
-                else:
-                    self.groups.append([n, ])
+                    n = self.befor_procesing.pop()
 
+                self.algorithm(n)
+
+    def algorithm(self, n):
+        n = n.lower().replace(" ", "").replace("\n", "")
+        for group in self.groups:
+            if all(fuzz.ratio(n, w) > 80 for w in group):
+                group.append(n)
+                break
+        else:
+            self.groups.append([n, ])
 
     def start(self):
-        mutex_befor_procesing = Lock()
-        reader = threading.Thread(target=self.input_file, args=[mutex_befor_procesing])
-        #reader = threading.Thread(target=self.input_file(mutex_befor_procesing), args=[mutex_befor_procesing])
-
-
-        mutex_after_procesing= Lock()
-        handler = threading.Thread(target=self.processing, args=[mutex_befor_procesing,mutex_after_procesing])
-
-
-        grouping =threading.Thread(target=self.algorithm, args=[mutex_after_procesing])
-
-        #handler = threading.Thread(target=self.processing(mutex_befor_procesing, mutex_after_procesing), args=[mutex_befor_procesing, mutex_after_procesing])
+        mutex = Lock()
+        reader = threading.Thread(target=self.input_file, args=[mutex])
+        # reader = threading.Thread(target=self.input_file(mutex_befor_procesing), args=[mutex_befor_procesing])
+        # handler= Process(target=self.processing, args=[mutex])
+        handler = threading.Thread(target=self.processing, args=[mutex])
+        # handler1 = threading.Thread(target=self.processing, args=[mutex_befor_procesing,mutex_after_procesing])
+        # handler = threading.Thread(target=self.processing(mutex_befor_procesing, mutex_after_procesing),
+        #                            args=[mutex_befor_procesing, mutex_after_procesing])
 
         reader.start()
+
         handler.start()
-        grouping.start()
-
-
+        # handler1.start()
 
         reader.join()
+
         handler.join()
-        grouping.join()
+        # handler1.join()
         print("before :\n{0}\n num:{1}".format(self.befor_procesing, len(self.befor_procesing)))
-        print("after :\n{0}\n num:{1}".format(self.after_procesing, len(self.after_procesing)))
-        c=0
+        c = 0
         for i in self.groups:
-            c+=len(i)
-        print("algorithm :\n{0}\nnum:{1}".format(self.groups,c))
+            c += len(i)
+        print("algorithm :\n{0}\nnum:{1}".format(self.groups, c))
 
 
 if __name__ == '__main__':
